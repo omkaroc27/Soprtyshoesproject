304e69f60c967a0e0375386b8de3cee59e133967
omkaroc27 <77641172+omkaroc27@users.noreply.github.com>
2021-07-10 23:56:52 +0530

    Add files via upload

---
+++ cartEx/src/main/java/com/Shoppingcart/Controller/Shoescart.java
@@ -0,0 +1,57 @@
+package com.Shoppingcart.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.Shoppingcart.dao.ProductbookDto;
+
+
+@Controller
+public class Shoescart {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		return "home";
+	}
+	@RequestMapping("/home")
+	public String home1() {
+		return"home";
+	}
+	@RequestMapping("/contact")
+	public String contact() {
+		return"contact";
+	}
+	@RequestMapping(value = "/order")
+	public String order() {
+		return "order";
+	}
+	
+	@RequestMapping("/Product")
+	public String home() {
+		return "Product";
+	}
+
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping("/probookform")
+		public String probookform() {
+			return "probookform";
+		}
+	
+	
+	@RequestMapping("/probookdetails")
+		public String probookdetails() {
+		
+		return "probookdetails";
+		
+			
+		}
+	}
+
